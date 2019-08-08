fc1af11ffe164a88506461b823a0faf661e0a25e
Eugene Khristo <marteau9292@gmail.com>
2019-08-08 11:04:41 +0300

    Testing subdocuments

--- users/test/subdocuments_test.js
+++ users/test/subdocuments_test.js
@@ -8,4 +8,29 @@ describe('Test subdocuments', () => {
     const user = await User.findOne({ name: 'Serj' });
     assert(user.posts[0].title === 'SOAD ROCKS!');
   });
+
+  it('Should add a new post subdocument to an existing User collection', async () => {
+    const serj = new User({ name: 'Serj' });
+    await serj.save();
+
+    let user = await User.findOne({ name: 'Serj' });
+    user.posts.push({ title: 'Arials in the Sky' });
+    await user.save();
+
+    user = await User.findOne({ name: 'Serj' });
+    assert(user.posts[0].title === 'Arials in the Sky');
+  });
+
+  it('should remove existing subdocument', async () => {
+    const serj = new User({ name: 'Serj', posts: [{ title: 'SOAD ROCKS!' }] });
+    await serj.save();
+
+    let user = await User.findOne({ name: 'Serj' });
+    // user.posts = user.posts.filter(post => post.title !== 'SOAD ROCKS!');
+    user.posts[0].remove();
+    await user.save();
+
+    user = await User.findOne({ name: 'Serj' });
+    assert(user.posts.length === 0);
+  });
 });
